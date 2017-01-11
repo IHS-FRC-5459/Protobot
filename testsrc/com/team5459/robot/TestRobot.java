dd0d0246f5e6fd2037c8dd627b68f5db0adf82fb
FilipKernan <filip.kernan@gmail.com>
2017-01-11 18:32:05 -0500

    hiihi

--- testsrc/com/team5459/robot/TestRobot.java
+++ testsrc/com/team5459/robot/TestRobot.java
@@ -1,25 +1,25 @@
 /* Created Mon Nov 28 16:30:42 EST 2016 */
 package com.team5459.robot;
 
-import static org.hamcrest.CoreMatchers.*;
-import org.junit.Assert;
-import org.junit.Test;
-import org.strongback.mock.Mock;
-import org.strongback.mock.MockSolenoid;
-import org.fest.assertions.Delta;
 import org.junit.Before;
-import org.strongback.drive.TankDrive;
-import org.strongback.components.ui.FlightStick;
-import org.strongback.components.Motor;
-import static org.fest.assertions.Assertions.assertThat;
 import org.junit.Ignore;
+import org.junit.Test;
+
 import org.strongback.command.CommandTester;
-import org.strongback.mock.MockController;
+import org.strongback.components.*;
+import org.strongback.drive.*;
+import org.strongback.mock.Mock;
+import org.strongback.mock.MockMotor;
+import org.strongback.mock.MockSolenoid;
+import static org.fest.assertions.Assertions.assertThat;
+
+
+
 
 
 public class TestRobot {
-    private Motor leftMotor;
-    private Motor rightMotor;
+    private MockMotor leftMotor;
+    private MockMotor rightMotor;
     private TankDrive drive;
     private CommandTester tester;
     
@@ -31,52 +31,51 @@ public class TestRobot {
         drive = new TankDrive(leftMotor, rightMotor);
 
     }
-        
+    @Ignore
     @Test
     public void shouldShiftUpWhenAlreadyShiftedUp(){
         MockSolenoid solenoid = Mock.manualSolenoid().extend();
         System.out.println("Solenoid created");
         ShiftUpCommand command = new ShiftUpCommand(solenoid);
-        
         assertThat(solenoid.isExtending()).isTrue();
         assertThat(solenoid.isRetracting()).isFalse();
         System.out.println("Command created");
         assertThat(command.execute()).isTrue();
         solenoid.stop();
-        assertThat(solenoid.isStopped()).isTrue(); 
+        assertThat(solenoid.isStopped()).isTrue();
         System.out.println("shouldShiftUpWhenAlreadyShiftedUp passed");
 }
 
-    @Ignore
+    
     @Test
     public void shouldShiftUpWhenShiftedDown(){
         MockSolenoid solenoid = Mock.manualSolenoid().retract();
         System.out.println("Solenoid created");
         ShiftUpCommand command = new ShiftUpCommand(solenoid);
         System.out.println("Solenoid created");
-        assertThat(command.execute()).isTrue();
         assertThat(solenoid.isExtending()).isTrue();
         assertThat(solenoid.isRetracting()).isFalse();
+        assertThat(command.execute()).isTrue();
         solenoid.stop();
         assertThat(solenoid.isStopped()).isTrue();   
         System.out.println("shouldShiftUpWhenShiftedDown passed");
 }
-    @Ignore
+    
     @Test
     public void shouldShiftDownWhenAlreadyShiftedUp(){
         MockSolenoid solenoid = Mock.manualSolenoid().extend();
         System.out.println("Solenoid created");
         ShiftDownCommand command = new ShiftDownCommand(solenoid);
         System.out.println("Command created");
-        assertThat(command.execute()).isTrue();
         assertThat(solenoid.isExtending()).isTrue();
         assertThat(solenoid.isRetracting()).isFalse();
+        assertThat(command.execute()).isTrue();
         solenoid.stop();
         assertThat(solenoid.isStopped()).isTrue();
         System.out.println("shouldShiftDownWhenAlreadyShiftedUp passed");
 }
 
-    @Ignore
+    
     @Test
     public void shouldShiftDownWhenAlreadyShiftedDown(){
         MockSolenoid solenoid = Mock.manualSolenoid().retract();
@@ -86,19 +85,18 @@ public class TestRobot {
         assertThat(solenoid.isExtending()).isTrue();
         assertThat(solenoid.isRetracting()).isFalse();
         assertThat(command.execute()).isTrue();
-
         solenoid.stop();
         assertThat(solenoid.isStopped()).isTrue();  
         System.out.println("shouldShiftDownWhenAlreadyShiftedDown passed");
 }
-    @Ignore
+    
     @Test
     public void shouldDriveForwardAtASpeed(){
         tester = new CommandTester(new DriveCommand(drive, 0.5,0.5));
         assertThat(leftMotor.getSpeed()).isEqualTo(0.0);
         assertThat(rightMotor.getSpeed()).isEqualTo(0.0);
         System.out.println("Inital Speed passed");
-        tester.step(2000);
+        tester.step(1000);
         assertThat(leftMotor.getSpeed()).isEqualTo(0.5);
         assertThat(rightMotor.getSpeed()).isEqualTo(0.5);
         System.out.println("Final Speed passed");
